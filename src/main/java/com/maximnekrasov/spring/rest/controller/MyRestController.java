f2323f1f34062a631a8bc6cd4c9a46297b79302a
nekras88 <nekras88@mail.ru>
2021-05-22 15:53:51 +0300

    Add get for id

--- src/main/java/com/maximnekrasov/spring/rest/controller/MyRestController.java
+++ src/main/java/com/maximnekrasov/spring/rest/controller/MyRestController.java
@@ -5,6 +5,7 @@ import com.maximnekrasov.spring.rest.entity.Employee;
 import com.maximnekrasov.spring.rest.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,4 +23,14 @@ public class MyRestController {
     return  allEmployees;
 
     }
+
+
+    @GetMapping("/employees/{id}")
+    public Employee getEmployee(@PathVariable int id){
+    Employee employee = employeeService.getEmployee(id);
+    return employee;
+
+
+    }
+
 }
